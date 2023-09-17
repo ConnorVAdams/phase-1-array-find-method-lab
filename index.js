892c299646657178a22f91d84ec584730f221b8b
Connor Adams <connorvadams@gmail.com>
2023-09-17 13:11:28 -0600

    superbowlWin() searches a team's record and returns a year they won, if applicable.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const superbowlWin = (record) => {
+const winningRecord = record.find(isWin);
+if (winningRecord) {
+    return winningRecord.year;
+    }    
+};
+
+const isWin = (element) => {
+return element.result === 'W';
+};
+
+superbowlWin(record);
